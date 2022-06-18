ad62951ccb989637dceb3f824c7a9a12cadda9d9
PhanHuuDat <phanhuudat0808@gmail.com>
2022-06-18 21:04:24 +0700

    update

---
+++ MVCMovie/Models/ViewModels/MovieVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCMovie.Models.ViewModels
+{
+    public class MovieVM
+    {
+        public GenreMovie MovieView { get; set; }
+        public Member Member { get; set; }
+        public RoleStatus RoleStatus { get; set; }
+
+        public IEnumerable<Genre> GenreList { get; set; }
+    }
+}
